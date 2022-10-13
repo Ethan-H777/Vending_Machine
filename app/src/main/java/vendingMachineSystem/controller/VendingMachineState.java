419ae66a1180d886fccf1c6d33e129178fffbfa6
Chris Gu <chrisgu@192-168-1-105.tpgi.com.au>
2022-10-13 22:58:03 +1100

    added dummy code for state timeout

--- app/src/main/java/vendingMachineSystem/controller/VendingMachineState.java
+++ app/src/main/java/vendingMachineSystem/controller/VendingMachineState.java
@@ -8,9 +8,18 @@ public abstract class VendingMachineState {
 	
 	public VendingMachineState(VendingMachine vm) {
 		this.vm = vm;
+		if (!(this instanceof DefaultState)) {
+			//TODO we can add a thread to monitor the timeout and switch states to the default state on timeout
+//			Thread thread = new Thread();
+//			thread.run();
+		}
 	}
 	
 	public abstract void run();
+	
+	public void inputEntered() {
+		//TODO this should be called after every action to reset the timeout period
+	}
 	
 	public String readInput() {
 		return "0";
