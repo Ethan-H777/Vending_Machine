c11e7575cc4c9fe0a260ce90aa34b3292bf3d657
nvas2758 <nvas2758@uni.sydney.edu.au>
2022-10-25 18:59:00 +1100

    text, csv and change report

--- app/src/main/java/vendingMachineSystem/controller/ReportsState.java
+++ app/src/main/java/vendingMachineSystem/controller/ReportsState.java
@@ -5,6 +5,10 @@ import vendingMachineSystem.VendingMachine;
 import vendingMachineSystem.view.LoggedInView;
 import vendingMachineSystem.view.ReportsView;
 
+import java.io.*;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
 public class ReportsState extends VendingMachineState {
 
         private String role;
@@ -25,4 +29,47 @@ public class ReportsState extends VendingMachineState {
         vm.setState( new LoggedInState(vm, role) );
     }
 
+    void output_csv(String filename, String plaintext){
+        File file = new File(filename);
+        try
+        {
+            file.createNewFile();
+            BufferedWriter file_w = new BufferedWriter(new FileWriter(filename, true));
+            file_w.append(plaintext);
+            file_w.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
+
+    public void changeReport(boolean is_csv) {
+        // colnames
+        String outString = "name,value,quantity\n";
+
+        // append contents
+        String[][] cashData = super.getCashData();
+        for ( int i =0; i < cashData.length; i++){
+            for ( int j = 0; j < cashData[i].length; j++){
+                outString = outString + cashData[i][j];
+                if ( j == cashData[i].length-1 ){ // last
+                    outString = outString + "\n";
+                } else{
+                    outString = outString + ",";
+                }
+            }
+
+        }
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        String filename = new SimpleDateFormat("yyyyMMdd HH:mm:ss.S").format(timestamp) + "_change";
+        if (is_csv){
+            filename = filename +".csv";
+        } else {
+            filename = filename + ".txt";
+        }
+        output_csv(filename, outString);
+    }
+
+
+}
--- app/src/main/java/vendingMachineSystem/view/ReportsView.java
+++ app/src/main/java/vendingMachineSystem/view/ReportsView.java
@@ -1,3 +1,4 @@
+
 package vendingMachineSystem.view;
 
 import vendingMachineSystem.controller.LoggedInState;
@@ -20,6 +21,9 @@ public class ReportsView extends AbstractView{
 
     @Override
     public void display() {
+        int report_stagger = 0;
+        int buttonWidth = 90;
+        Dimension reportDim;
         Window window = Window.getInstance();
 
         Panel p = new Panel();
@@ -27,11 +31,40 @@ public class ReportsView extends AbstractView{
 
         // label
         JLabel label = new JLabel(label_string);
-        Dimension size = label.getPreferredSize();
-        label.setBounds(0,0,size.width,size.height);
+        Dimension top_size = label.getPreferredSize();
+        label.setBounds(0,0,top_size.width,top_size.height);
+        report_stagger += top_size.height;
         p.add(label);
 
         // report buttons
+
+        // change
+        if (state.getRole().equals("OWNER") || state.getRole().equals("SELLER")) {
+            JButton reportButtonCSV = new JButton("csv"); // button
+            reportButtonCSV.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    ReportsView.this.state.changeReport(true);
+                }
+            });
+            JLabel reportLabel = new JLabel("Change"); // label
+            reportDim = reportLabel.getPreferredSize(); // txt
+            JButton reportButtonTXT = new JButton("text");
+            reportButtonTXT.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    ReportsView.this.state.changeReport(false);
+                }
+            });
+
+            reportLabel.setBounds(0, report_stagger, reportDim.width, reportDim.height);
+            reportButtonCSV.setBounds(reportDim.width + 10, report_stagger, buttonWidth, reportDim.height);
+            reportButtonTXT.setBounds(reportDim.width + 20 + buttonWidth, report_stagger, buttonWidth, reportDim.height);
+            p.add(reportLabel);
+            p.add(reportButtonCSV);
+            p.add(reportButtonTXT);
+            report_stagger += reportDim.height;
+        }
 
         // cancel
         JButton cancelButton = new JButton("Cancel");
